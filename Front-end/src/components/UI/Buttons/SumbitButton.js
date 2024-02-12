e2eda550b78be96dbe411a1eef50359001497244
Mohamed TRAORE <momogafou85@gmail.com>
2024-02-13 00:39:22 +0100

    mern course application

---
+++ Front-end/src/components/UI/Buttons/SumbitButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SumbitButton = (props) => (
+  <button className={props.className} type="sumbit">
+    {props.Label}
+  </button>
+);
+
+export default SumbitButton;
